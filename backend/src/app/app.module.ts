5b86c0fc826c4622c6f13eb4d4880aaa1c036da8
tezzash <tezzashdadhich@gmail.com>
2026-08-13 12:05:32 +0530

    feat: wire player service into app module

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { AppConfig, ConfigService } from '../config';
 import { HealthService } from '../health';
+import { PlayerService } from '../player';
 
 export class AppModule {
   readonly config: AppConfig;
   readonly healthService: HealthService;
+  readonly playerService: PlayerService;
 
   constructor(readonly configService = new ConfigService()) {
     this.config = this.configService.load();
     this.healthService = new HealthService(this.config);
+    this.playerService = new PlayerService();
   }
 }
