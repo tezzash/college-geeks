58c2ccf21475b46928acd4b9d8a543a6c7f804d9
tezzash <tezzashdadhich@gmail.com>
2026-08-13 12:27:32 +0530

    feat: export persistent gameplay services

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,2 +1,6 @@
+export * from './database.allies.service';
+export * from './database.battle.service';
+export * from './database.jobs.service';
 export * from './database.player.service';
+export * from './database.tower.service';
 export * from './prisma.service';
