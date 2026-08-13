9b73b3f58e31c97641a8716a2531c9cd1ae86fc5
tezzash <tezzashdadhich@gmail.com>
2026-08-13 12:26:43 +0530

    feat: wire database services into app module

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,12 +1,17 @@
+import { DatabasePlayerService, PrismaService } from '../database';
 import { AppConfig, ConfigService } from '../config';
 import { HealthService } from '../health';
 
 export class AppModule {
   readonly config: AppConfig;
   readonly healthService: HealthService;
+  readonly prisma: PrismaService;
+  readonly databasePlayerService: DatabasePlayerService;
 
   constructor(readonly configService = new ConfigService()) {
     this.config = this.configService.load();
     this.healthService = new HealthService(this.config);
+    this.prisma = new PrismaService();
+    this.databasePlayerService = new DatabasePlayerService(this.prisma);
   }
 }
