8258e3b033173edf6d2dd2c3b86289e44fbc4d9b
tezzash <tezzashdadhich@gmail.com>
2026-08-13 12:27:49 +0530

    feat: expose persistent gameplay services

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { DatabasePlayerService, PrismaService } from '../database';
+import { DatabaseAlliesService, DatabaseBattleService, DatabaseJobsService, DatabasePlayerService, DatabaseTowerService, PrismaService } from '../database';
 import { AppConfig, ConfigService } from '../config';
+import { CombatService } from '../combat';
+import { GameConfigService } from '../game-config';
 import { HealthService } from '../health';
 
 export class AppModule {
@@ -7,11 +9,27 @@ export class AppModule {
   readonly healthService: HealthService;
   readonly prisma: PrismaService;
   readonly databasePlayerService: DatabasePlayerService;
+  readonly databaseJobsService: DatabaseJobsService;
+  readonly databaseTowerService: DatabaseTowerService;
+  readonly databaseAlliesService: DatabaseAlliesService;
+  readonly databaseBattleService: DatabaseBattleService;
 
   constructor(readonly configService = new ConfigService()) {
     this.config = this.configService.load();
     this.healthService = new HealthService(this.config);
     this.prisma = new PrismaService();
     this.databasePlayerService = new DatabasePlayerService(this.prisma);
+    this.databaseJobsService = new DatabaseJobsService(this.prisma);
+    this.databaseTowerService = new DatabaseTowerService(this.prisma);
+    this.databaseAlliesService = new DatabaseAlliesService(this.prisma);
+
+    const gameConfig = new GameConfigService().getConfig();
+    const combat = new CombatService(gameConfig);
+    this.databaseBattleService = new DatabaseBattleService(
+      this.prisma,
+      combat,
+      gameConfig.pvpEnergyCost,
+      gameConfig.stealRate,
+    );
   }
 }
